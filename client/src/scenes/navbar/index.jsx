454a0d973ebf2ee8a161eed08f600887b3622faa
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-03-30 10:34:02 -0400

    styling fixes on the navbar

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -21,7 +21,7 @@ import {
   Close,
 } from "@mui/icons-material";
 import { useDispatch, useSelector } from "react-redux";
-import { setMode, setLogout } from "state";
+import { setMode, setLogout } from "../../state/index";
 import { useNavigate } from "react-router-dom";
 import FlexBetween from "../../components/FlexBetween";
 import { dark } from "@mui/material/styles/createPalette";
@@ -39,7 +39,7 @@ const Navbar = () => {
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
 
-  const fullName = `${user.firstName} ${user.lastName}`;
+  const fullName = "Yves"; //`${user.firstName} ${user.lastName}`;
 
   return (
     <FlexBetween padding={"1rem 6%"} backgroundColor={alt}>
@@ -55,7 +55,9 @@ const Navbar = () => {
               cursor: "pointer",
             },
           }}
-        ></Typography>
+        >
+          SWACO
+        </Typography>
         {isNonMobileScreens && (
           <FlexBetween
             backgroundColor={neutralLight}
@@ -117,76 +119,76 @@ const Navbar = () => {
       )}
 
       {/* MOBILE NAV */}
-      {!isNonMobileScreens &&
-        isMobileMenuToggled(
-          <Box
-            position={"fixed"}
-            right={"0"}
-            bottom={"0"}
-            height={"100%"}
-            zIndex={"10"}
-            maxWidth={"500px"}
-            minWidth={"300px"}
-            backgroundColor={background}
-          >
-            <Box display={"flex"} justifyContent={"flex-end"} p={"1rem"}>
-              <IconButton
-                onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
-              >
-                <Close />
-              </IconButton>
-            </Box>
-
-            {/* MENU ITEMS */}
-            <FlexBetween
-              display={"flex"}
-              flexDirection={"column"}
-              justifyContent={"center"}
-              alignItems={"center"}
-              gap={"3rem"}
+      {!isNonMobileScreens && isMobileMenuToggled && (
+        <Box
+          position={"fixed"}
+          right={"0"}
+          bottom={"0"}
+          height={"100%"}
+          zIndex={"10"}
+          maxWidth={"500px"}
+          minWidth={"300px"}
+          backgroundColor={background}
+        >
+          {/* CLOSE ICON */}
+          <Box display={"flex"} justifyContent={"flex-end"} p={"1rem"}>
+            <IconButton
+              onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
             >
-              <IconButton
-                onClick={() => dispatch(setMode())}
-                sx={{ fontSize: "25px" }}
-              >
-                {theme.palette.mode === "dark" ? (
-                  <DarkMode sx={{ fontSize: "25px" }} />
-                ) : (
-                  <LightMode sx={{ color: dark, fontSize: "25px" }} />
-                )}
-              </IconButton>
-              <Message sx={{ fontSize: "25px" }} />
-              <Notifications sx={{ fontSize: "25px" }} />
-              <Help sx={{ fontSize: "25px" }} />
-              <FormControl variant="standard" value={fullName}>
-                <Select
-                  value={fullName}
-                  sx={{
-                    backgroundColor: neutralLight,
-                    width: "150px",
-                    borderRadius: "0.25rem",
-                    p: "0.25rem 1rem",
-                    "& .MuiSvgIcon-root": {
-                      pr: "0.25rem",
-                      width: "3rem",
-                    },
-                    "& .MuiSelect-select:focus": {
-                      backgroundColor: neutralLight,
-                    },
-                  }}
-                  Input={<InputBase />}
-                >
-                  <MenuItem value={fullName}>
-                    <Typography>{fullName}</Typography>
-                  </MenuItem>
-                  <MenuItem onClick={() => dispatch(setLogout())}>
-                    Log Out
-                  </MenuItem>
-                </Select>
-              </FormControl>
-            </FlexBetween>
+              <Close />
+            </IconButton>
           </Box>
-        )}
+
+          {/* MENU ITEMS */}
+          <FlexBetween
+            display={"flex"}
+            flexDirection={"column"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            gap={"3rem"}
+          >
+            <IconButton
+              onClick={() => dispatch(setMode())}
+              sx={{ fontSize: "25px" }}
+            >
+              {theme.palette.mode === "dark" ? (
+                <DarkMode sx={{ fontSize: "25px" }} />
+              ) : (
+                <LightMode sx={{ color: dark, fontSize: "25px" }} />
+              )}
+            </IconButton>
+            <Message sx={{ fontSize: "25px" }} />
+            <Notifications sx={{ fontSize: "25px" }} />
+            <Help sx={{ fontSize: "25px" }} />
+            <FormControl variant="standard" value={fullName}>
+              <Select
+                value={fullName}
+                sx={{
+                  backgroundColor: neutralLight,
+                  width: "150px",
+                  borderRadius: "0.25rem",
+                  p: "0.25rem 1rem",
+                  "& .MuiSvgIcon-root": {
+                    pr: "0.25rem",
+                    width: "3rem",
+                  },
+                  "& .MuiSelect-select:focus": {
+                    backgroundColor: neutralLight,
+                  },
+                }}
+                Input={<InputBase />}
+              >
+                <MenuItem value={fullName}>
+                  <Typography>{fullName}</Typography>
+                </MenuItem>
+                <MenuItem onClick={() => dispatch(setLogout())}>
+                  Log Out
+                </MenuItem>
+              </Select>
+            </FormControl>
+          </FlexBetween>
+        </Box>
+      )}
     </FlexBetween>
   );
 };
