7a5be18da7cbabecc4b4a91d50102b2ae08dc5a9
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-03-30 10:53:48 -0400

    add form and registration schema using yup

---
+++ client/src/scenes/loginPage/Form.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  TextField,
+  useMediaQuery,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import { ModeEditOutline } from "@mui/icons-material";
+import { Formik } from "formik"; //Form library
+import * as yup from "yup"; //Validation library
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setLogin } from "../../state";
+import DropZone from "react-dropzone";
+import FlexBetween from "../../components/FlexBetween";
+
+const registerSchema = yup.object().shape({
+  firstName: yup.string().required("required"),
+  lastName: yup.string().required("required"),
+  phone: yup.string().phone("invalid phone number").required("required"),
+  location: yup.string().required("required"),
+  picture: yup.string().required("required"),
+});
+
+const Form = () => {
+  return <div></div>;
+};
+
+export default Form;
