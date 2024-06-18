121972260017e2db90a3a27b7edf1b9a69af5cec
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-06-18 07:23:32 -0400

    add the login function

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -81,6 +81,26 @@ const Form = () => {
     }
   };
 
+  const login = async (values, onsubmitProps) => {
+    const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(values),
+    });
+
+    const loggedIn = await loggedInResponse.json();
+    onsubmitProps.resetForm();
+    if (loggedIn) {
+      dispatch(
+        setLogin({
+          user: loggedIn.user,
+          token: loggedIn.token,
+        })
+      );
+      navigate("/home");
+    }
+  };
+
   const handleFormSubmit = async (values, onsubmitProps) => {
     if (isLogin) await login(values, onsubmitProps);
     if (isRegister) await register(values, onsubmitProps);
