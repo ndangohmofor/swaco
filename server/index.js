f0013ab650d3ce3bd0920ed1fd17f669c73ba275
I M Ndangoh <ndangohmofor@gmail.com>
2024-06-19 17:08:17 -0400

    add the file storage and middleware configs

--- server/index.js
+++ server/index.js
@@ -12,3 +12,27 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 
 /** MIDDLEWARE AND PACKAGE CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '30mb' }));
+app.use(morgan('common'));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(cors());
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+app.use(multer().any());
+
+/** FILE STORAGE CONFIGURATIONS */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/assets');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
