67fb94177a278c2858c4b0698b5c43357ec519f5
I M Ndangoh <ndangohmofor@gmail.com>
2024-06-19 23:00:08 -0400

    fixed the connection to remote mongodb

--- server/index.js
+++ server/index.js
@@ -40,13 +40,11 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 //** MONGOOSE CONFIGURATIONS */
+const MONGO_URL = process.env.MONGO_URL;
 const PORT = process.env.PORT || 5001;
 mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(MONGO_URL)
   .then(() => {
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
   })
-  .catch((error) => console.log(`${error} did not connect`));
+  .catch((error) => console.log(`${error} - did not connect`));
