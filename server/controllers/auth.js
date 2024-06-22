a95b472334fd255fd38ba041b2d559f67f19ac7c
I M Ndangoh <ndangohmofor@gmail.com>
2024-06-21 20:58:00 -0400

    add otp-generator and register user and send otp code

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,6 +3,7 @@ import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 import Status from '../models/Status.js';
 import Role from '../models/Role.js';
+import otpGenerator from 'otp-generator';
 
 /** REGISTER USER */
 export const register = async (req, res) => {
@@ -16,6 +17,14 @@ export const register = async (req, res) => {
       return;
     }
 
+    let otp = otpGenerator.generate(5, {
+      digits: true,
+      alphabets: false,
+      specialChars: false,
+      upperCase: false,
+    });
+    const hashedOtp = await bcrypt.hash(otp, 10);
+
     const newUser = new User({
       firstName,
       lastName,
@@ -26,9 +35,25 @@ export const register = async (req, res) => {
       role: Role.Guest,
       picturePath,
     });
+
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+
+  let smsSent = false;
+  //Send the OTP to the user
+
+  if (smsSent) {
+    return res.status(200).json({
+      success: true,
+      message: 'OTP sent successfully via SMS',
+    });
+  } else {
+    return res.status(500).json({
+      success: false,
+      message: 'Failed to send OTP via SMS',
+    });
   }
 };
