92e96fb217dc3bcfd0422e846fedf0de52b097d3
I M Ndangoh <ndangohmofor@gmail.com>
2024-06-21 21:29:24 -0400

    import register controller function as a name export instead of default export

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ import dotenv from 'dotenv';
 import multer from 'multer';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import { register } from './controllers/auth.js';
 
 /** MIDDLEWARE AND PACKAGE CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
