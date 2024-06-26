9ea851712d34847766dffb34fedc9b7e6347c100
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-06-25 23:54:51 -0400

    setup the module to send sms via aws pinpoint

--- server/util/send-sms.js
+++ server/util/send-sms.js
@@ -4,4 +4,12 @@ const aws_region = 'us-east-1';
 const origNumber = process.env.ORIGINATION_NUMBER;
 const languageCode = 'en-US';
 
-const voidId = 'SWACO';
+const voidId = 'Ruth';
+
+const ssmlMessage =
+  '<speak>' +
+  'This is a test message sent from <emphasis>Amazon Pinpoint</emphasis> ' +
+  "using the <break strength='weak'/>AWS SDK for JavaScript in Node.js. " +
+  "<amazon:effect phonation='soft'>Thank you for listening." +
+  '</amazon:effect>' +
+  '</speak>';
