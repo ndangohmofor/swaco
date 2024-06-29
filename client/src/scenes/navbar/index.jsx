ed3a75b5f004f4f4f45683dc52d2f88016233d79
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-06-29 15:07:41 -0400

    assign username as Guest if user is undefined

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
 
-  const fullName = "Yves"; //`${user.firstName} ${user.lastName}`;
+  const fullName = !!user ? `${user.firstName} ${user.lastName}` : "Guest";
 
   return (
     <FlexBetween padding={"1rem 6%"} backgroundColor={alt}>
