e9a6b134b59b83a5fe67287a965759d8edcc7890
I M Ndangoh <ndangohmofor@gmail.com>
2024-06-30 15:48:42 -0400

    add a carousel of photos to the second snap section

--- client/src/scenes/LandingPage/index.jsx
+++ client/src/scenes/LandingPage/index.jsx
@@ -4,6 +4,32 @@ import { Box, Card, CardMedia, Container, Paper } from "@mui/material";
 import Carousel from "react-material-ui-carousel";
 
 const Index = () => {
+  const photos = [
+    {
+      img: "/assets/photos/mobile/1.png",
+      alt: "Beautiful Swaco ladies",
+    },
+    {
+      img: "/assets/photos/mobile/2.png",
+      alt: "Swaco meeting in progress",
+    },
+    {
+      img: "/assets/photos/mobile/3.png",
+      alt: "Swaco group photo",
+    },
+    {
+      img: "/assets/photos/mobile/4.png",
+      alt: "Swaco women in solidarity",
+    },
+    {
+      img: "/assets/photos/mobile/5.png",
+      alt: "Swaco men behind the women",
+    },
+    {
+      img: "/assets/photos/mobile/6.png",
+      alt: "Swaco meeting item 11",
+    },
+  ];
   return (
     <>
       <Box>
@@ -36,6 +62,25 @@ const Index = () => {
               muted
             />
           </Card>
+        </Paper>
+        <Paper
+          sx={{
+            height: "100vh",
+            scrollSnapAlign: "start",
+          }}
+        >
+          <Carousel>
+            {photos.map((item, index) => (
+              <Card key={index}>
+                <CardMedia
+                  component="img"
+                  image={item.img}
+                  alt={item.alt}
+                  sx={{ height: "100vh", width: "100%" }}
+                />
+              </Card>
+            ))}
+          </Carousel>
         </Paper>
       </Container>
     </>
