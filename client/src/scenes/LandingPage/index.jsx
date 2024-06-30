08ae0e57885ea856a80f88465aed4d99da6f7608
I M Ndangoh <ndangohmofor@gmail.com>
2024-06-30 16:03:29 -0400

    Adding buttons on the carousel items

--- client/src/scenes/LandingPage/index.jsx
+++ client/src/scenes/LandingPage/index.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import Navbar from "../navbar";
-import { Box, Card, CardMedia, Container, Paper } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardMedia,
+  Container,
+  Paper,
+  Stack,
+} from "@mui/material";
 import Carousel from "react-material-ui-carousel";
 
 const Index = () => {
@@ -8,28 +16,35 @@ const Index = () => {
     {
       img: "/assets/photos/mobile/1.png",
       alt: "Beautiful Swaco ladies",
+      cardAction: "About us",
     },
     {
       img: "/assets/photos/mobile/2.png",
       alt: "Swaco meeting in progress",
+      cardAction: "Our Members",
     },
     {
       img: "/assets/photos/mobile/3.png",
       alt: "Swaco group photo",
+      cardAction: "Our Ex Co",
     },
     {
       img: "/assets/photos/mobile/4.png",
       alt: "Swaco women in solidarity",
+      cardAction: "Our Constitution",
     },
     {
       img: "/assets/photos/mobile/5.png",
       alt: "Swaco men behind the women",
+      cardAction: "Becoming a member",
     },
     {
       img: "/assets/photos/mobile/6.png",
       alt: "Swaco meeting item 11",
+      cardAction: "Contact us",
     },
   ];
+
   return (
     <>
       <Box>
