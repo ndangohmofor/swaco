22fdeb158908e4d6666d313efde510b171c38ccc
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-06-29 21:28:30 -0400

    add snapscrolling to the landing page

--- client/src/scenes/LandingPage/index.jsx
+++ client/src/scenes/LandingPage/index.jsx
@@ -1,12 +1,49 @@
 import React from "react";
 import Navbar from "../navbar";
-import { Box } from "@mui/material";
+import { Box, Card, CardMedia, Container } from "@mui/material";
 
 const Index = () => {
   return (
-    <Box>
-      <Navbar />
-    </Box>
+    <>
+      <Box>
+        <Navbar />
+      </Box>
+      <Container
+        sx={{
+          height: "100vh",
+          overflow: "scroll",
+          scrollSnapType: "y mandatory",
+        }}
+      >
+        <Box
+          sx={{
+            height: "100vh",
+            scrollSnapAlign: "start",
+            alignItems: "center",
+            justifyContent: "center",
+            borderBottom: "1px solid lightgrey",
+          }}
+        >
+          <CardMedia
+            component={"video"}
+            className="mobile-video"
+            src="/assets/placeholder_video.mp4"
+            autoPlay
+            loop
+            muted
+          />
+        </Box>
+        <Card
+          sx={{
+            height: "100vh",
+            scrollSnapAlign: "start",
+            alignItems: "center",
+            justifyContent: "center",
+            borderBottom: "1px solid lightgrey",
+          }}
+        ></Card>
+      </Container>
+    </>
   );
 };
 
