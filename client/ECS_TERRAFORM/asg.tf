ff966724db21d7c5d8d675d7b8cd6c6523b39360
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-09-02 23:44:33 -0400

    configure the auto scaling group terraform file

--- client/ECS_TERRAFORM/asg.tf
+++ client/ECS_TERRAFORM/asg.tf
@@ -1,4 +1,4 @@
-data "aws_ami" "amazon-linux-2" {
+data "aws_ami" "amazon-linux" {
     most_recent = true
 
     filter {
@@ -38,4 +38,39 @@ resource "aws_security_group" "ec2-sg" {
   tags = {
     project = "swaco"
   }
+}
+
+resource "aws_launch_configuration" "lc" {
+  name = "prod-ecs"
+  image_id = data.aws_ami.amazon-linux.id
+  instance_type = "t2.micro"
+  lifecycle {
+    create_before_destroy = true
+  }
+  iam_instance_profile = aws_iam_instance_profile.ecs_service_role.name
+  key_name = var.key_name
+  security_groups = [aws_security_group.ec2-sg.id]
+  associate_public_ip_address = true
+  user_data = <<EOF
+    #!/bin/bash
+    sudo apt-get update
+    sudo echo "ECS_CLUSTER=${var.cluster_name}" >> /etc/ecs/ecs.config
+  EOF
+}
+
+resource "aws_autoscaling_group" "asg" {
+    name = "prod-asg"
+    launch_configuration = aws_launch_configuration.lc.name
+    min_size = 1
+    max_size = 3
+    desired_capacity = 2
+    health_check_type = "ELB"
+    health_check_grace_period = 300
+    vpc_zone_identifier = module.vpc.public_subnets
+
+    target_group_arns = [aws_lb_target_group.aws_lb_target_group.arn]
+    protect_from_scale_in = true
+    lifecycle {
+      create_before_destroy = true
+    }
 }
