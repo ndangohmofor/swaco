8e4b80df84e806c34f5e628b1f9bc7121e7b17a8
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-09-03 21:59:39 -0400

    add defaults in variables for cluster name and key name

--- client/ECS_TERRAFORM/variable.tf
+++ client/ECS_TERRAFORM/variable.tf
@@ -1,9 +1,11 @@
 variable "key_name" {
   type = string
   description = "the name for the ssh key used for aws_launch_configuration"
+  default = "swaco-ssh-key"
 }
 
 variable "cluster_name" {
   type = string
   description = "the name for the ECS cluster"
+  default = "swaco-cluster"
 }
