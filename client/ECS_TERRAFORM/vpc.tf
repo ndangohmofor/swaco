68af700e0777932bdf04d8b3110aeaddf3a8fbe3
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-09-03 22:07:06 -0400

    update the version of terraform to 5.0.0

--- client/ECS_TERRAFORM/vpc.tf
+++ client/ECS_TERRAFORM/vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
-  version = "2.77.0"
+  version = "5.0.0"
   name = "swaco-vpc"
   cidr = "10.0.0.0/16"
   azs = ["us-east-1a", "us-east-1b"]
