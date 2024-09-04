b2eea1c8f0e29eddf2f36ea70c67a30ae9775e85
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-09-03 21:22:44 -0400

    add the provider and terraform blocks in main file

--- client/ECS_TERRAFORM/main.tf
+++ client/ECS_TERRAFORM/main.tf
@@ -1,12 +1,11 @@
 provider "aws" {
   region = "us-east-1"
-  version = "~> 3.0"
 }
 
 terraform {
   backend "s3" {
     bucket = "swacobucket"
     key = "state/terraform.tfstate"
-    region = "us-east-1"
+    
   }
 }
