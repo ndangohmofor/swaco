fb3d149b89e6b75b898bddc7fa40fe41811b2552
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-09-10 08:02:25 -0400

    add a route53 record for www.swaco.us

--- client/ECS_TERRAFORM/dns.tf
+++ client/ECS_TERRAFORM/dns.tf
@@ -1,18 +1,29 @@
 resource "aws_route53_zone" "swaco-us" {
   name = "swaco.us"
   tags = {
-    "env" = "prod"
+    "env"     = "prod"
     "project" = "swaco"
   }
 }
 
 resource "aws_route53_record" "swaco-us-root" {
   zone_id = aws_route53_zone.swaco-us.zone_id
-  name = "swaco.us"
-  type = "A"
+  name    = "swaco.us"
+  type    = "A"
   alias {
-    name = aws_alb.prod-alb.dns_name
-    zone_id = aws_alb.prod-alb.zone_id
+    name                   = aws_alb.prod-alb.dns_name
+    zone_id                = aws_alb.prod-alb.zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "www-swaco-us" {
+  zone_id = aws_route53_zone.swaco-us.zone_id
+  name    = "www.swaco.us"
+  type    = "A"
+  alias {
+    name                   = aws_alb.prod-alb.dns_name
+    zone_id                = aws_alb.prod-alb.zone_id
     evaluate_target_health = true
   }
 }
