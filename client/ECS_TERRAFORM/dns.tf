80047b07b974c1482923d0509d7b95ee8d7036ff
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-09-10 07:58:43 -0400

    add route 53 record for the root domain

--- client/ECS_TERRAFORM/dns.tf
+++ client/ECS_TERRAFORM/dns.tf
@@ -5,3 +5,14 @@ resource "aws_route53_zone" "swaco-us" {
     "project" = "swaco"
   }
 }
+
+resource "aws_route53_record" "swaco-us-root" {
+  zone_id = aws_route53_zone.swaco-us.zone_id
+  name = "swaco.us"
+  type = "A"
+  alias {
+    name = aws_alb.prod-alb.dns_name
+    zone_id = aws_alb.prod-alb.zone_id
+    evaluate_target_health = true
+  }
+}
