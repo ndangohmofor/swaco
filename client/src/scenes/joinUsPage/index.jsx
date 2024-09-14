6a248429dfe916d95840d1a03a335857a8777d0a
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-09-14 13:06:24 -0400

    add a join us page

---
+++ client/src/scenes/joinUsPage/index.jsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material";
+import React from "react";
+import Navbar from "../navbar";
+
+const JoinUs = () => {
+  return (
+    <>
+      <Box>
+        <Navbar />
+      </Box>
+    </>
+  );
+};
+
+export default JoinUs;
