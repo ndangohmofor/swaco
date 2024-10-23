0aa07e5097f0e56665379aef5cf51972e247a034
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-10-22 22:27:15 -0400

    add method to verify the otp and respond back to the user with success or effor message

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,5 @@
 import User from '../models/User.js';
-import { sendOtp } from '../util/send-sms.js';
+import { sendOtp, verifyOtp } from '../util/send-sms.js';
 
 /** REGISTER USER */
 export const register = async (req, res) => {
@@ -26,6 +26,34 @@ export const register = async (req, res) => {
     console.log('newUser', newUser);
     await newUser.save();
     res.status(201).json({ referenceId });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+//* VERIFY OTP */
+export const confirmOtp = async (req, res) => {
+  try {
+    const { referenceId, phoneNumber, otpCode } = req.body;
+
+    const user = await User.findOne({ phoneNumber });
+    if (!user) {
+      res.status(404).json({ message: 'User not found' });
+      return;
+    }
+
+    const { success, message } = await verifyOtp(
+      referenceId,
+      phoneNumber,
+      otpCode,
+    );
+
+    if (!success) {
+      res.status(400).json({ message });
+      return;
+    }
+
+    res.status(200).json({ success, message });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
