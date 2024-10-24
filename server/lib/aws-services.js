5eb67656cf558ad70e557a2139eb876caa984c7f
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-10-24 05:27:19 -0400

    refactor aws files

--- server/lib/aws-services.js
+++ server/lib/aws-services.js
@@ -1,4 +1,4 @@
 import { PinpointClient } from '@aws-sdk/client-pinpoint';
 
 const aws_region = process.env.AWS_REGION || 'us-east-1';
-export const pinPoint = new PinpointClient({ region: aws_region });
+export const pinpoint = new PinpointClient({ region: aws_region });
