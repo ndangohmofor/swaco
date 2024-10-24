0555c9c2a7101afd683cd71f6f5ffe32e61fcf95
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-10-24 05:56:52 -0400

    setup the credentials using fromIni in the pinpoint client

--- server/lib/aws-services.js
+++ server/lib/aws-services.js
@@ -1,4 +1,9 @@
 import { PinpointClient } from '@aws-sdk/client-pinpoint';
+import { fromIni } from '@aws-sdk/credential-providers';
 
 const aws_region = process.env.AWS_REGION || 'us-east-1';
-export const pinpoint = new PinpointClient({ region: aws_region });
+
+export const credentials = fromIni({
+  profile: 'default',
+});
+export const pinpoint = new PinpointClient({ region: aws_region, credentials });
